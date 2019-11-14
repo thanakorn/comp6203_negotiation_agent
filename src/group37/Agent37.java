419619b27d7d117847f3a1e1da21cc89b288c023
thanakorn <thanakorn.panyapiang@outlook.com>
2019-11-14 22:11:16 +0000

    Setup project

---
+++ src/group37/Agent37.java
@@ -0,0 +1,18 @@
+package group37;
+
+import genius.core.actions.Action;
+import genius.core.parties.AbstractNegotiationParty;
+
+import java.util.List;
+
+public class Agent37 extends AbstractNegotiationParty {
+    @Override
+    public Action chooseAction(List<Class<? extends Action>> possibleActions) {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
