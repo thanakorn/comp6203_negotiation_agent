749b4dadc48830dd4af432d1c58862d5ecb5e632
thanakorn <thanakorn.panyapiang@outlook.com>
2019-11-28 10:17:42 +0000

    Generate coefficients and deltaU

--- src/main/java/group37/preference/lp/LinearPMSolver.java
+++ src/main/java/group37/preference/lp/LinearPMSolver.java
@@ -14,30 +14,52 @@ import java.util.List;
 public class LinearPMSolver {
 
     private HashMap<Value, Integer> valueIndices;
+    private List<Double[]> deltaU;
 
     public LinearPMSolver(Domain domain, List<Bid> bidOrder){
         valueIndices = new HashMap<>();
         List<Value> allValues = new ArrayList<Value>();
+
         for (Issue i : domain.getIssues()){
             allValues.addAll(((IssueDiscrete)i).getValues());
         }
+
         for(int i = 0; i < allValues.size(); i++){
             valueIndices.put(allValues.get(i), i);
         }
+
+        deltaU = generateDeltaU(bidOrder);
     }
 
     public int getValueIndex(Value v){
         return valueIndices.get(v);
     }
 
-//    public double[] getCoefficientMatrix(Bid bid){
-//        int totalOptions = bid.getIssues().stream().mapToInt(i -> ((IssueDiscrete)i).getValues().size()).sum();
-//        double[] coefficients = new double[totalOptions];
-//        Arrays.fill(coefficients, 0.0f);
-//        bid.getIssues().stream().forEach(i -> {
-//            bid.getValue(i);
-//        });
-//        return coefficients;
-//    }
+    public List<Double[]> getDeltaU(){ return deltaU; }
+
+    private List<Double[]> generateDeltaU(List<Bid> bidOrder){
+        deltaU = new ArrayList<>();
+        for(int i = bidOrder.size() - 1; i > 0; i--){
+            deltaU.add(getDeltaCoefficients(bidOrder.get(i), bidOrder.get(i - 1)));
+        }
+        return deltaU;
+    }
+
+    public Double[] getDeltaCoefficients(Bid a, Bid b){
+        double[] aCoef = getBidCoefficients(a);
+        double[] bCoef = getBidCoefficients(b);
+        Double[] delta = new Double[aCoef.length];
+        Arrays.setAll(delta, n -> aCoef[n] - bCoef[n]);
+        return delta;
+    }
+
+    public double[] getBidCoefficients(Bid bid){
+        double[] coefficients = new double[valueIndices.keySet().size()];
+        for (Issue i : bid.getIssues()){
+            Value v = bid.getValue(i);
+            coefficients[getValueIndex(v)] = 1.0;
+        }
+        return coefficients;
+    }
 
 }
--- src/test/java/group37/preference/lp/LinearPMSolverSpec.java
+++ src/test/java/group37/preference/lp/LinearPMSolverSpec.java
@@ -4,17 +4,17 @@ import genius.core.Bid;
 import genius.core.Domain;
 import genius.core.issue.Issue;
 import genius.core.issue.IssueDiscrete;
+import genius.core.issue.Value;
 import genius.core.issue.ValueDiscrete;
-import genius.core.uncertainty.User;
-import genius.core.uncertainty.UserModel;
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LinearPMSolverSpec {
 
@@ -33,4 +33,77 @@ public class LinearPMSolverSpec {
         assertEquals(3, s.getValueIndex(new ValueDiscrete("A")));
         assertEquals(4, s.getValueIndex(new ValueDiscrete("B")));
     }
+
+    @Test
+    public void testGetBidCoefficients(){
+        Mockito.when(mockDomain.getIssues()).thenReturn(issues);
+        LinearPMSolver s = new LinearPMSolver(mockDomain, new ArrayList<Bid>());
+        Bid bid1 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("1"));
+            put(2, new ValueDiscrete("A"));
+        }});
+        Bid bid2 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("3"));
+            put(2, new ValueDiscrete("B"));
+        }});
+        Bid bid3 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("2"));
+            put(2, new ValueDiscrete("A"));
+        }});
+        assertTrue(Arrays.equals(new double[]{1.0, 0.0, 0.0, 1.0, 0.0}, s.getBidCoefficients(bid1)));
+        assertTrue(Arrays.equals(new double[]{0.0, 0.0, 1.0, 0.0, 1.0}, s.getBidCoefficients(bid2)));
+        assertTrue(Arrays.equals(new double[]{0.0, 1.0, 0.0, 1.0, 0.0}, s.getBidCoefficients(bid3)));
+    }
+
+    @Test
+    public void testGenerateDeltaCoefficients(){
+        Mockito.when(mockDomain.getIssues()).thenReturn(issues);
+        Bid bid1 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("1"));
+            put(2, new ValueDiscrete("A"));
+        }});
+        Bid bid2 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("3"));
+            put(2, new ValueDiscrete("B"));
+        }});
+        Bid bid3 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("2"));
+            put(2, new ValueDiscrete("A"));
+        }});
+        LinearPMSolver s = new LinearPMSolver(mockDomain, new ArrayList<Bid>(){{
+            add(bid1);
+            add(bid2);
+        }});
+        assertTrue(Arrays.equals(new Double[]{1.0, 0.0, -1.0, 1.0, -1.0}, s.getDeltaCoefficients(bid1, bid2)));
+        assertTrue(Arrays.equals(new Double[]{1.0, -1.0, 0.0, 0.0, 0.0}, s.getDeltaCoefficients(bid1, bid3)));
+        assertTrue(Arrays.equals(new Double[]{0.0, -1.0, 1.0, -1.0, 1.0}, s.getDeltaCoefficients(bid2, bid3)));
+        assertTrue(Arrays.equals(new Double[]{-1.0, 1.0, 0.0, 0.0, 0.0}, s.getDeltaCoefficients(bid3, bid1)));
+    }
+
+    @Test
+    public void testConstructorGenerateDeltaU(){
+        Mockito.when(mockDomain.getIssues()).thenReturn(issues);
+        Bid bid1 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("1"));
+            put(2, new ValueDiscrete("A"));
+        }});
+        Bid bid2 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("3"));
+            put(2, new ValueDiscrete("B"));
+        }});
+        Bid bid3 = new Bid(mockDomain, new HashMap<Integer, Value>() {{
+            put(1, new ValueDiscrete("2"));
+            put(2, new ValueDiscrete("A"));
+        }});
+        LinearPMSolver s = new LinearPMSolver(mockDomain, new ArrayList<Bid>(){{
+            add(bid3);
+            add(bid2);
+            add(bid1);
+        }});
+        List<Double[]> deltaU = s.getDeltaU();
+        assertEquals(2, s.getDeltaU().size());
+        assertTrue(Arrays.equals(new Double[]{1.0, 0.0, -1.0, 1.0, -1.0}, deltaU.get(0)));
+        assertTrue(Arrays.equals(new Double[]{0.0, -1.0, 1.0, -1.0, 1.0}, deltaU.get(1)));
+    }
+
 }
