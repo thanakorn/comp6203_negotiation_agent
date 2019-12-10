1961d2927fb8a60e0e8782e92f7808b80fa826e4
thanakorn <thanakorn.panyapiang@outlook.com>
2019-12-10 15:30:06 +0000

    Test SolverFactory2

--- src/main/java/group37/preference/lp/LinearPMBuilder.java
+++ src/main/java/group37/preference/lp/LinearPMBuilder.java
@@ -1,132 +1,132 @@
-//package group37.preference.lp;
-//
-//import genius.core.Bid;
-//import genius.core.Domain;
-//import genius.core.issue.Issue;
-//import genius.core.issue.IssueDiscrete;
-//import genius.core.issue.Value;
-//import genius.core.uncertainty.BidRanking;
-//import scpsolver.constraints.LinearBiggerThanEqualsConstraint;
-//import scpsolver.constraints.LinearConstraint;
-//import scpsolver.constraints.LinearEqualsConstraint;
-//import java.util.*;
-//
-///**
-// * Build equations for Linear Programming Preference Model
-// */
-//public class LinearPMBuilder {
-//
-//    private int numValues;
-//    private HashMap<Value, Integer> valueIndices;
-//    private double[][] deltaU;
-//    private int numSlackVar;
-//    private BidRanking bidRank;
-//
-//    public LinearPMBuilder(Domain domain, BidRanking bidRank){
-//        List<Bid> bidOrder = bidRank.getBidOrder();
-//        valueIndices = new HashMap<>();
-//        List<Value> allValues = new LinkedList<>();
-//
-//        for (Issue i : domain.getIssues()){
-//            allValues.addAll(((IssueDiscrete)i).getValues());
-//        }
-//        numValues = allValues.size();
-//        for(int i = 0; i < allValues.size(); i++){
-//            valueIndices.put(allValues.get(i), i);
-//        }
-//
-//        if(bidOrder.size() > 0) deltaU = generateDeltaU(bidOrder);
-//        numSlackVar = bidOrder.size() - 1;
-//        this.bidRank = bidRank;
-//    }
-//
-//    public double[] getObjective(){
-//        double[] objective = new double[numValues + numSlackVar];
-//        for(int i = 0; i < numValues + numSlackVar; i++){
-//            if(i < numValues) objective[i] = 0.0;
-//            else objective[i] = 1.0;
-//        }
-//        return objective;
-//    }
-//
-//    public List<LinearConstraint> getConstraints(){
-//        List<LinearConstraint> constraints = new LinkedList<>();
-//        int length = numValues + numSlackVar;
-//        double[] emptyConstraint = new double[length];
-//        Arrays.fill(emptyConstraint, 0.0);
-//        // z >= 0
-//        for(int i = 0; i < numSlackVar; i++){
-//            double[] c = emptyConstraint.clone();
-//            c[numValues + i] = 1.0;
-//            constraints.add(new LinearBiggerThanEqualsConstraint(c, 0.0, "Z" + i));
-//        }
-//        // deltaU + z >= 0
-//        for(int i = 0; i < numSlackVar; i++){
-//            double[] c = emptyConstraint.clone();
-//            double[] delta = deltaU[i];
-//            System.arraycopy(delta, 0, c, 0, numValues);
-//            c[numValues + i] = 1.0;
-//            constraints.add(new LinearBiggerThanEqualsConstraint(c, 0.0, "DELTA" + i));
-//        }
-//
-//        // u(x) >= 0
-//        for(int i = 0; i < numValues; i++){
-//            double[] c = emptyConstraint.clone();
-//            c[i] = 1.0;
-//            constraints.add(new LinearBiggerThanEqualsConstraint(c, 0.0, "U" + i));
-//        }
-//
-//        // U(maxBid) = bidRank.getHighUtility
-//        List<Bid> bidOrder = bidRank.getBidOrder();
-//        Bid highBid = bidOrder.get(bidOrder.size() - 1);
-//        double[] cHighBid = emptyConstraint.clone();
-//        for(Issue i: highBid.getIssues()){
-//            Value v = highBid.getValue(i);
-//            cHighBid[valueIndices.get(v)] = 1.0;
-//        }
-//        constraints.add(new LinearEqualsConstraint(cHighBid, bidRank.getHighUtility(), "UMAX"));
-//
-//        // U(minBid) = bidRank.getLowUtility
-//        Bid lowBid = bidOrder.get(0);
-//        double[] cLowBid = emptyConstraint.clone();
-//        for(Issue i: lowBid.getIssues()){
-//            Value v = lowBid.getValue(i);
-//            cLowBid[valueIndices.get(v)] = 1.0;
-//        }
-//        constraints.add(new LinearEqualsConstraint(cLowBid, bidRank.getLowUtility(), "UMIN"));
-//
-//        return constraints;
-//    }
-//
-//    public int getValueIndex(Value v){
-//        return valueIndices.get(v);
-//    }
-//
-//    public double[][] getDeltaU(){ return deltaU; }
-//
-//    private double[][] generateDeltaU(List<Bid> bidOrder){
-//        deltaU = new double[bidOrder.size() - 1][numValues];
-//        for(int i = bidOrder.size() - 1, j = 0; i > 0; i--, j++){
-//            deltaU[j] = getDeltaCoefficients(bidOrder.get(i), bidOrder.get(i - 1));
-//        }
-//        return deltaU;
-//    }
-//
-//    public double[] getDeltaCoefficients(Bid a, Bid b){
-//        double[] aCoef = getBidCoefficients(a);
-//        double[] bCoef = getBidCoefficients(b);
-//        double[] delta = new double[aCoef.length];
-//        Arrays.setAll(delta, n -> aCoef[n] - bCoef[n]);
-//        return delta;
-//    }
-//
-//    public double[] getBidCoefficients(Bid bid){
-//        double[] coefficients = new double[numValues];
-//        for (Issue i : bid.getIssues()){
-//            Value v = bid.getValue(i);
-//            coefficients[valueIndices.get(v)] = 1.0;
-//        }
-//        return coefficients;
-//    }
-//
-//}
+package group37.preference.lp;
+
+import genius.core.Bid;
+import genius.core.Domain;
+import genius.core.issue.Issue;
+import genius.core.issue.IssueDiscrete;
+import genius.core.issue.Value;
+import genius.core.uncertainty.BidRanking;
+import scpsolver.constraints.LinearBiggerThanEqualsConstraint;
+import scpsolver.constraints.LinearConstraint;
+import scpsolver.constraints.LinearEqualsConstraint;
+import java.util.*;
+
+/**
+ * Build equations for Linear Programming Preference Model
+ */
+public class LinearPMBuilder {
+
+    private int numValues;
+    private HashMap<Value, Integer> valueIndices;
+    private double[][] deltaU;
+    private int numSlackVar;
+    private BidRanking bidRank;
+
+    public LinearPMBuilder(Domain domain, BidRanking bidRank){
+        List<Bid> bidOrder = bidRank.getBidOrder();
+        valueIndices = new HashMap<>();
+        List<Value> allValues = new LinkedList<>();
+
+        for (Issue i : domain.getIssues()){
+            allValues.addAll(((IssueDiscrete)i).getValues());
+        }
+        numValues = allValues.size();
+        for(int i = 0; i < allValues.size(); i++){
+            valueIndices.put(allValues.get(i), i);
+        }
+
+        if(bidOrder.size() > 0) deltaU = generateDeltaU(bidOrder);
+        numSlackVar = bidOrder.size() - 1;
+        this.bidRank = bidRank;
+    }
+
+    public double[] getObjective(){
+        double[] objective = new double[numValues + numSlackVar];
+        for(int i = 0; i < numValues + numSlackVar; i++){
+            if(i < numValues) objective[i] = 0.0;
+            else objective[i] = 1.0;
+        }
+        return objective;
+    }
+
+    public List<LinearConstraint> getConstraints(){
+        List<LinearConstraint> constraints = new LinkedList<>();
+        int length = numValues + numSlackVar;
+        double[] emptyConstraint = new double[length];
+        Arrays.fill(emptyConstraint, 0.0);
+        // z >= 0
+        for(int i = 0; i < numSlackVar; i++){
+            double[] c = emptyConstraint.clone();
+            c[numValues + i] = 1.0;
+            constraints.add(new LinearBiggerThanEqualsConstraint(c, 0.0, "Z" + i));
+        }
+        // deltaU + z >= 0
+        for(int i = 0; i < numSlackVar; i++){
+            double[] c = emptyConstraint.clone();
+            double[] delta = deltaU[i];
+            System.arraycopy(delta, 0, c, 0, numValues);
+            c[numValues + i] = 1.0;
+            constraints.add(new LinearBiggerThanEqualsConstraint(c, 0.0, "DELTA" + i));
+        }
+
+        // u(x) >= 0
+        for(int i = 0; i < numValues; i++){
+            double[] c = emptyConstraint.clone();
+            c[i] = 1.0;
+            constraints.add(new LinearBiggerThanEqualsConstraint(c, 0.0, "U" + i));
+        }
+
+        // U(maxBid) = bidRank.getHighUtility
+        List<Bid> bidOrder = bidRank.getBidOrder();
+        Bid highBid = bidOrder.get(bidOrder.size() - 1);
+        double[] cHighBid = emptyConstraint.clone();
+        for(Issue i: highBid.getIssues()){
+            Value v = highBid.getValue(i);
+            cHighBid[valueIndices.get(v)] = 1.0;
+        }
+        constraints.add(new LinearEqualsConstraint(cHighBid, bidRank.getHighUtility(), "UMAX"));
+
+        // U(minBid) = bidRank.getLowUtility
+        Bid lowBid = bidOrder.get(0);
+        double[] cLowBid = emptyConstraint.clone();
+        for(Issue i: lowBid.getIssues()){
+            Value v = lowBid.getValue(i);
+            cLowBid[valueIndices.get(v)] = 1.0;
+        }
+        constraints.add(new LinearEqualsConstraint(cLowBid, bidRank.getLowUtility(), "UMIN"));
+
+        return constraints;
+    }
+
+    public int getValueIndex(Value v){
+        return valueIndices.get(v);
+    }
+
+    public double[][] getDeltaU(){ return deltaU; }
+
+    private double[][] generateDeltaU(List<Bid> bidOrder){
+        deltaU = new double[bidOrder.size() - 1][numValues];
+        for(int i = bidOrder.size() - 1, j = 0; i > 0; i--, j++){
+            deltaU[j] = getDeltaCoefficients(bidOrder.get(i), bidOrder.get(i - 1));
+        }
+        return deltaU;
+    }
+
+    public double[] getDeltaCoefficients(Bid a, Bid b){
+        double[] aCoef = getBidCoefficients(a);
+        double[] bCoef = getBidCoefficients(b);
+        double[] delta = new double[aCoef.length];
+        Arrays.setAll(delta, n -> aCoef[n] - bCoef[n]);
+        return delta;
+    }
+
+    public double[] getBidCoefficients(Bid bid){
+        double[] coefficients = new double[numValues];
+        for (Issue i : bid.getIssues()){
+            Value v = bid.getValue(i);
+            coefficients[valueIndices.get(v)] = 1.0;
+        }
+        return coefficients;
+    }
+
+}
